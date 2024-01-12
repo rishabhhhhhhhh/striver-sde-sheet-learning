6f1bbf79cc348c7dc12499d2d65fd94fb019ba06
rishabhhhhhhhh <p.rishabh.arora@gmail.com>
2024-01-12 22:02:10 +0530

    add q link

--- dsalgo/ll/del_node.cpp
+++ dsalgo/ll/del_node.cpp
@@ -1,4 +1,5 @@
 //delete node whose reference is given (not head or tail node)
+//https://www.codingninjas.com/studio/problems/1105578?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
 void deleteNode(LinkedListNode<int> * node) {
     LinkedListNode<int> * nextNode = node->next;
     node->data = nextNode->data;
